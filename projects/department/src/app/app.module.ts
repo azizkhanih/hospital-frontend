43e90e336729729a2a8d289df46b0ef3fb55c2f5
h.azizkhani <azizkhani.develop@gmail.com>
2021-09-24 14:16:56 +0330

    feat: add departments and department pages

--- projects/department/src/app/app.module.ts
+++ projects/department/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DepartmentComponent } from './pages/department/department.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
